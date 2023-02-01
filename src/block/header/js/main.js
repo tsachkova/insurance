79cc95bca11ecfb2539fbcbfc365eb42492ee409
tsachkova <tsachkova@gmail.com>
2023-02-01 14:46:46 +0200

    2

--- src/block/header/js/main.js
+++
@@ -1,9 +0,0 @@
-let menuButton = document.querySelector('.menu__button');
-let menu = document.querySelector('.menu__list');
-
-menuButton.addEventListener('click', e => {
-
-    if (e.target == menuButton) {
-        menu.classList.toggle('open');
-    }
-})
